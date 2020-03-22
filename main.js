6b88330c0137ec70d3e3b468b61f0002045a2964
Arkadiusz Wójcik <arkadiuszwojcik15@wp.pl>
2020-03-22 12:59:27 +0100

    Error correction and add function for single words

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+const input = document.getElementById("inputValue");
+const btn = document.querySelector("button");
+const span = document.getElementById("results");
 
+let value = "";
+
+input.addEventListener("input", e => {
+  value = e.target.value;
+  value = value.trim().toLowerCase();
+  value = [...value];
+});
+
+btn.addEventListener("click", checkWords);
+function checkWords() {
+  const copyArray = [...value].reverse();
+  let counterGodLetter = 0;
+  copyArray.forEach((letter, index) => {
+    if (letter === value[index]) {
+      counterGodLetter++;
+    } else return;
+  });
+  value.length === counterGodLetter
+    ? (span.textContent = "Słowo jest palindromem")
+    : (span.textContent = "Słowo nie jest palindromem");
+}
