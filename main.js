ac1f4ac7f67dfa8b1b6d40d169512243dd1a7406
Arkadiusz Wójcik <arkadiuszwojcik15@wp.pl>
2020-03-23 17:51:18 +0100

    The function checks the whole sentence

--- main.js
+++ main.js
@@ -2,25 +2,31 @@ const input = document.getElementById("inputValue");
 const btn = document.querySelector("button");
 const span = document.getElementById("results");
 
-let value = "";
+let orginalValue = "";
 
 input.addEventListener("input", e => {
-  value = e.target.value;
-  value = value.trim().toLowerCase();
-  value = [...value];
+  orginalValue = e.target.value;
+  orginalValue = orginalValue.trim().toLowerCase();
+  orginalValue = [...orginalValue];
 });
 
 btn.addEventListener("click", checkWords);
 function checkWords() {
-  const copyArray = [...value].reverse();
+  let name = "";
+  let copyValueReverse = [...orginalValue].reverse();
+  let copyValue = orginalValue.filter(element => element != " ");
+  copyValueReverse = copyValueReverse.filter(element => element != " ");
   let counterGodLetter = 0;
-  copyArray.forEach((letter, index) => {
-    if (letter === value[index]) {
+  copyValueReverse.forEach((letter, index) => {
+    if (letter === copyValue[index]) {
       counterGodLetter++;
     } else return;
   });
-  if (!value) return;
-  value.length === counterGodLetter
-    ? (span.textContent = `Słowo: ${value.join("")} jest palindromem`)
-    : (span.textContent = `Słowo: ${value.join("")} nie jest palindromem`);
+  if (!orginalValue) return alert("Wrowadź słowo.");
+  if (orginalValue.includes(" ")) name = "Zdanie";
+  else name = "Słowo";
+  copyValue.length === counterGodLetter
+    ? (span.textContent = `${name}: ${orginalValue.join("")} jest palindromem`)
+    : (span.textContent = `${name}: ${orginalValue.join("")} nie jest palindromem`);
+  input.value = "";
 }
